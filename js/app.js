7ac41c2ba476ae581ff73d729d0fc0a12995e63c
David Chris Baldwin <shorerydr10@live.com>
2014-07-14 14:04:45 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
-$( "#ventures>div>ul>li" )
+$( "#ventures>div>ul>.grid-25" )
 .mouseenter(function() {
     $( this ).find(".hover").stop().animate({top: "0"}, 150);
 });
-$( "#ventures>div>ul>li" )
+$( "#ventures>div>ul>.grid-25" )
 .mouseleave(function() {
 	    $( this ).parent().find(".hover").stop().animate({top: "-100%"});
 });
@@ -10,11 +10,11 @@ $( "#ventures>div>ul>li" )
 
 
 
-$( "#team>div>ul>li>div" )
+$( "#team>div>ul>.grid-25>div" )
 .mouseenter(function() {
     $( this ).find(".hover").stop().animate({top: "0"}, 150);
 });
-$( "#team>div>ul>li>div" )
+$( "#team>div>ul>.grid-25>div" )
 .mouseleave(function() {
 	    $( this ).parent().find(".hover").stop().animate({top: "-100%"});
 });
